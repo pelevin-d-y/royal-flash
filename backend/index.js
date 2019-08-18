5390cec919e658c5b39bbb8427629ad8564bbf2e
pelevin-d-y <natpuot1992@gmail.com>
2019-08-18 13:49:33 +0300

    added 3 fields

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const Koa = require("koa");
 const Router = require("koa-router");
 const BodyParser = require("koa-bodyparser");
 const data = require("./localization")
+const ObjectID = require("mongodb").ObjectID;
 
 const app = new Koa();
 const router = new Router();
@@ -14,7 +15,8 @@ require("./mongo")(app);
 const filteredData = data.map((el) => {
     return {
         id: el['№'],
-        name: el['ФИО']
+        name: el['ФИО'],
+        registred: false
     }
 })
 
@@ -32,6 +34,35 @@ router.get("/people", async (ctx) => {
 
     countDocuments = await app.people.countDocuments()
     ctx.body = await ctx.app.people.find().toArray();
+});
+
+// Get one
+router.get("/people/:id", async (ctx) => {
+    ctx.body = await ctx.app.people.findOne({"id": ctx.params.id});
+});
+
+router.post("/people/:id", async (ctx) => {
+    let documentQuery = {"id": ctx.params.id}; // Used to find the document
+    const currentDocument = await ctx.app.people.findOne(documentQuery)
+    console.log('currentDocument.registred', currentDocument.registred)
+    if (currentDocument.registred === false) {
+        let valuesToUpdate = ctx.request.body;
+        // { "registred": true, "field1": "крутой1", "field2": "крутой2", "field3": "крутой3"}
+        await ctx.app.people.updateOne(documentQuery, {$set: valuesToUpdate});
+        ctx.body = {
+            response: {
+                status: 'true',
+                description: 'updated'
+            }
+        }
+    } else {
+        ctx.body = {
+            response: {
+                status: 'true',
+                description: 'replay'
+            }
+        }
+    }
 });
 
 const logger = require('koa-logger');
